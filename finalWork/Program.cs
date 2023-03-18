6c27685e430ec6665e0a42f409424190b48a8fb5
Milena <bagramanmilena@gmail.com>
2023-03-18 21:28:11 +0300

    created an array that calculates the number of elements by condition for a future sample array

--- finalWork/Program.cs
+++ finalWork/Program.cs
@@ -6,6 +6,14 @@
 
 string [] array = new string [6] {"Work", "IT",":D", "aaaaaaaaaaaa", "12!/13", "ye"};
 
-int 
+int NumberElements (string [] arr, int minСharacters)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+       if  (arr[i].Length <= minСharacters) count+=1;
+    }
+    return count;
+}
 
 Console.WriteLine("Имеющийся массив: ");
